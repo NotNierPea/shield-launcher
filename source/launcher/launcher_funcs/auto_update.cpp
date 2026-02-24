52065cf1e51be5dc214bbd8f8f1fb63783343484
Pea <130810875+NotNierPea@users.noreply.github.com>
2026-02-24 22:55:50 +0300

    update

--- source/launcher/launcher_funcs/auto_update.cpp
+++ source/launcher/launcher_funcs/auto_update.cpp
@@ -13,7 +13,7 @@
 #include <filesystem>
 
 namespace updater {
-    const std::string SERVER_VERSION = "1.0.1";
+    const std::string SERVER_VERSION = "1.0.2";
 
     const std::string GITHUB_API_URL = "https://api.github.com/repos/NotNierPea/shield-launcher/releases/latest";
     const std::string DOWNLOAD_URL_BASE = "https://github.com/NotNierPea/shield-launcher/releases/download/";
